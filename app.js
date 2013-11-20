f2636cbab155d434e6f05d7bcfd4b3907d401431
Sumeet Rohatgi <sum.rohatgi@gmail.com>
2013-11-19 19:42:24 -0800

    changed some

--- app.js
+++ app.js
@@ -32,10 +32,30 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-httpProxy.createServer(function(req, res, proxy) {
-  proxy.proxyRequest(req, res, { host: 'na1.salesforce.com', port: 443 });
-}).listen(3030);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+var server = httpProxy.createServer(function(req, res, proxy) {
+  //if (req.headers["X-Howl-Sid"] === null) {
+    // curl https://login.salesforce.com/services/oauth2/token 
+    // -d "grant_type=password" 
+    // -d 'client_id=3MVG9QDx8IX8nP5SFJKf4fpPHVMwt.HN4JWdanXhf.ipMW11xdJyeHFScCK_VCq.OSU0gPXFAl1wHazLqGIc.' 
+    // -d 'client_secret=6325945293227652987' 
+    // -d 'username=sumeet_rohatgi@hotmail.com' 
+    // -d 'password=test123442Fx59RN27Z00NBDfdbcuOSv'
+    /*req.write("grant_type=password&" + 
+      "client_id=3MVG9QDx8IX8nP5SFJKf4fpPHVMwt.HN4JWdanXhf.ipMW11xdJyeHFScCK_VCq.OSU0gPXFAl1wHazLqGIc.&" + 
+      "client_secret=6325945293227652987" + 
+      "username=sumeet_rohatgi@hotmail.com" + 
+      "password=test123442Fx59RN27Z00NBDfdbcuOSv"
+      );*/
+    //req.write('');
+    //req.end();
+    proxy.proxyRequest(req, res, { host: 'login.salesforce.com', port: 443 });
+  //}
 });
+
+server.proxy.on('end', function(req,res) {
+  res.header("Access-Control-Allow-Origin", "*");
+});
+
+server.listen(3030);
+console.log('Express server listening on port ' + 3030);
+
